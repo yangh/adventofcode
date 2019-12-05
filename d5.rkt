a9fd3d996c5e45aeaa588f051cefaecaf63fdf36
Walter H. Yang <yangh.cn@gmail.com>
2019-12-05 23:52:36 +0800

    Add day 5 part 2 code

--- d5.rkt
+++ d5.rkt
@@ -4,7 +4,8 @@
 
 (define (input->vector input)
   (list->vector
-   (string-split input ",")))
+   (map string-trim
+        (string-split input ","))))
 
 ;(displayln (format "Code length: ~a" (vector-length ins)))
 
@@ -16,9 +17,13 @@
   (number-at ints (number-at ints pos)))
 
 (define (load-param ints op pos nth)
-  (if (or (< (string-length op) (+ 2 nth))
-          (char=? #\0 (string-ref op (- (string-length op) nth 2))))
+  (if (or
+       ; No mode in opcode, Position mode as default
+       (< (string-length op) (+ 2 nth))
+       ; 0 - Possition mode
+       (char=? #\0 (string-ref op (- (string-length op) nth 2))))
       (value-at ints (+ pos nth))
+      ; 1 - Immediate mode
       (number-at ints (+ pos nth))))
       
 (define (op12 func ints pos)
@@ -29,43 +34,117 @@
     (displayln (format "OP: ~a ~a ~a ~a" op n1 n2 n3))
     (vector-set! ints n3 (format "~a" (func n1 n2)))))
 
-; Only 1 input required
-(define input-1 "1")
+(define (value-set! ints pos value)
+  (displayln (format "Set: ~a ~a" pos value))
+  (vector-set! ints pos value))
 
-(define (op3 ints pos ID)
-  (vector-set! ints
-               (number-at ints (+ pos 1))
-               ID))
+(define (op3 ints pos value)
+  (value-set! ints (number-at ints (+ pos 1)) value))
 
 (define (op4 ints pos)
   (let* ([op (vector-ref ints pos)]
          [n1 (load-param ints op pos 1)])
     (displayln (format "op4: ~a" n1))))
 
-(define (intcode ints pos ID)
+; jump-if-ture/false
+(define (jmp-if cond ints pos)
+  (let* ([op (vector-ref ints pos)]
+         [n1 (load-param ints op pos 1)]
+         [n2 (load-param ints op pos 2)])
+    (displayln (format "jump-if: ~a ~a" n1 n2))
+    (if (cond n1 0) n2 0)))
+
+(define ne? (lambda (n1 n2) (not (= n1 n2))))
+
+(define (op5 ints pos)
+  (jmp-if ne? ints pos))
+
+(define (op6 ints pos)
+  (jmp-if = ints pos))
+
+(define (jmp-op ints pos op)
+  (let ([ret (op ints pos)])
+    (if (= ret 0)
+        (intcode ints (+ pos 3))
+        (intcode ints ret))))
+
+; compare, lt, eq
+(define (cmp-if cond ints pos)
+  (let* ([op (vector-ref ints pos)]
+         [n1 (load-param ints op pos 1)]
+         [n2 (load-param ints op pos 2)])
+    (displayln (format "cmp-if: ~a ~a" n1 n2))
+    (cond n1 n2)))
+
+(define (op7 ints pos)
+  (cmp-if < ints pos))
+
+(define (op8 ints pos)
+  (cmp-if = ints pos))
+
+(define (cmp-op ints pos op)
+  (let ([n3 (number-at ints (+ pos 3))]
+        [value (if (op ints pos) "1" "0")])
+    (value-set! ints n3 value)
+    (intcode ints (+ pos 4))))
+
+(define system-id "1")
+
+(define (is-op? op code)
+  (char=? (last (string->list op)) code))
+
+(define (intcode ints pos)
   (when (< pos (vector-length ints))
     (let ([op (vector-ref ints pos)])
+      ;(displayln (format "~a" ints))
+      (displayln (format "pos: ~a" pos))
+      (displayln (format "opc: ~a" op))
       (cond
         [(string=? op "99")
-         ;(displayln "99")
          ;(displayln ints)
-         #f
-         ]
-        [(char=? (last (string->list op)) #\1)
+         #f]
+        [(is-op? op #\1)
          (op12 + ints pos)
-         (intcode ints (+ pos 4) ID)]
-        [(char=? (last (string->list op)) #\2)
+         (intcode ints (+ pos 4))]
+        [(is-op? op #\2)
          (op12 * ints pos)
-         (intcode ints (+ pos 4) ID)]
+         (intcode ints (+ pos 4))]
         [(string=? op "3")
-         (op3 ints pos ID)
-         (intcode ints (+ pos 2) ID)]
-        [(char=? (last (string->list op)) #\4)
+         (op3 ints pos system-id)
+         (intcode ints (+ pos 2))]
+        [(is-op? op #\4)
          (op4 ints pos)
-         (intcode ints (+ pos 2) ID)]
+         (intcode ints (+ pos 2))]
+        [(is-op? op #\5)
+         (jmp-op ints pos op5)]
+        [(is-op? op #\6)
+         (jmp-op ints pos op6)]
+        [(is-op? op #\7)
+         (cmp-op ints pos op7)]
+        [(is-op? op #\8)
+         (cmp-op ints pos op8)]
         [else
          (displayln (format "Unknown opcode: ~a" (vector-ref ints pos)))]))))
 
-; Part 1
-(intcode (input->vector input) 0 "1")
+(define (test input id)
+  (displayln (format "Input: ~a, ID: ~a" input id))
+  (set! system-id (format "~a" id))
+  (intcode (input->vector input) 0))
 
+; Part 1
+;(intcode (input->vector input) 0)
+
+; Part 2
+(test input 5)
+
+(define test2 "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
+
+;(test "3,9,8,9,10,9,4,9,99,-1,8" 7)
+;(test "3,9,8,9,10,9,4,9,99,-1,8" 8)
+;(test "3,9,8,9,10,9,4,9,99,-1,8" 9)
+
+;(test test2 7)
+;(test test2 8)
+;(test test2 9)
