c500ccd5ab469cbbb221360b1fd80d9df87882c1
Walter H. Yang <yangh.cn@gmail.com>
2019-12-09 21:32:38 +0800

    Add day 8 part 1 code

---
+++ d8.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(require "input.rkt")
+
+(define input (first (input-load-lines 8)))
+
+(define min-z 100000)
+(define min-l 0)
+(define n1 0)
+(define n2 0)
+
+(define (count-of c str)
+  (foldl (lambda (x r)
+           (+ r (if (char=? x c) 1 0)))
+         0 (string->list str)))
+
+(let ([ln (string-length input)])
+  (for ([i (range (/ ln (* 25 6)))])
+    (let* ([s1 (* i (* 25 6))]
+           [s2 (* (add1 i) (* 25 6))]
+           [str (substring input s1 s2)])
+      (let ([nz (count-of #\0 str)])
+        (displayln (format "Layer: ~a, n-of-zeror: ~a" i nz))
+        (when (< nz min-z)
+          (set! n1 (count-of #\1 str))
+          (set! n2 (count-of #\2 str))
+          (set! min-z nz)
+          (set! min-l i))))))
+
+min-z
+min-l
+n1
+n2
+(* n1 n2)
