e559028639a4909898a7e58163be4e9e9774e8e9
Walter H. Yang <yangh.cn@gmail.com>
2019-12-12 10:44:45 +0800

    Update day 11 code

--- d11.rkt
+++ d11.rkt
@@ -5,15 +5,6 @@
 
 (define input (first (input-load-lines 11)))
 
-(define d #\^)
-(define p 100)
-(define x (/ p 2))
-(define y (/ p 2))
-(define panelv (make-vector (* p p) 0))
-(define panels (make-hasheqv))
-
-(define (current-idx) (+ x (* p y)))
-
 (define turn-l #hasheqv((#\< . #\v)
                         (#\v . #\>)
                         (#\> . #\^)
@@ -26,22 +17,38 @@
                         (#\v . ( 0  1))
                         (#\> . ( 1  0))
                         (#\^ . ( 0 -1))))
-(define (turn t)
-  (let* ([turn-h (if (= t 0) turn-l turn-r)]
-         [dir (hash-ref turn-h d)]
-         [mov (hash-ref move-h dir)])
-    ;(displayln (format "Move: ~a,~a ~a ~a, ~a" x y mov dir (vector-ref panelv (current-idx))))
-    (set! d dir)
-    (set! x (+ x (list-ref mov 0)))
-    (set! y (+ y (list-ref mov 1)))))
 
-(define (part1 input)
+(define (part1 input n)
+  (define d #\^) ; Current direction
+  (define p 100) ; Panel size, estimated
+  (define x (/ p 2)) ; Current position
+  (define y (/ p 2))
+  (define panelv (make-vector (* p p) 0)) ; Record panel color info
+  (define panels (make-hasheqv)) ; Record painted panels
+
+  (define (current-idx) (+ x (* p y)))
+
+  (define (turn t)
+    (when (not (or (= t 0) (= t 1)))
+      (displayln (format "ERROR: unknown turn: ~a" t)))
+    (let* ([turn-h (if (= t 0) turn-l turn-r)]
+           [dir (hash-ref turn-h d)]
+           [mov (hash-ref move-h dir)])
+      ;(displayln (format "Move: ~a,~a ~a ~a, ~a" x y mov dir (vector-ref panelv (current-idx))))
+      (set! d dir)
+      (set! x (+ x (list-ref mov 0)))
+      (set! y (+ y (list-ref mov 1)))))
+
   (define ic (new Intcode%))
   (send ic set-pause-on-output #t)
   ;(send ic set-debug #t)
   (send ic load-code input)
 
-  (let loop ([i 0])
+  ; Init color
+  (vector-set! panelv (current-idx) n)
+
+  ; Paint loop
+  (let loop ([i n])
     (send ic set-input i)
     (let ([idx (current-idx)])
       (send ic run)
@@ -54,16 +61,21 @@
     (turn (send ic get-output))
     (when (not (send ic is-halt?))
       (loop (vector-ref panelv (current-idx)))))
-  (displayln (format "painted: ~a" (hash-count panels)))
+
+  ; Paint Result
   (for ([y (range 0 p)])
     (for ([x (range 0 p)])
       (let ([c (vector-ref panelv (+ x (* p y)))]
             [ped (hash-has-key? panels (+ x (* p y)))])
         (cond
           [(= c 1) (display "#")]
-          [(and (= c 0) ped) (display "o")]
+          ;[(and (= c 0) ped) (display "o")]
           [else (display ".")])))
     (displayln ""))
-  )
 
-(part1 input)
+  (displayln (format "painted: ~a" (hash-count panels))))
+
+(part1 input 0)
+
+; Part2
+(part1 input 1)
