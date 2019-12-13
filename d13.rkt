8769e15a597d12b7e5e8567d9ae3ce7988573132
Walter H. Yang <yangh.cn@gmail.com>
2019-12-14 01:44:18 +0800

    Bug in feaa1ba7b is not true, d13.rkt has bug

--- d13.rkt
+++ d13.rkt
@@ -9,38 +9,38 @@
   (define ic (new Intcode%))
   (send ic set-pause-on-output #t)
   ;(send ic set-debug #t)
-  (send ic load-code input)
 
   (define (read-i)
     (send ic run)
     (send ic get-output))
 
-  (define h 0)
-  (define v 0)
+  (define h 50)
+  (define v 50)
   (define nblks 0)
-  (define stage (make-vector 1600 0))
+  (define stage (make-vector 1000 0))
 
+  (send ic load-code input)
   (let loop ()
     ; Make sure use let* to eval one by one
     (define x (read-i))
     (define y (read-i))
     (define c (read-i))
-    (set! h (max h x))
-    (set! v (max v y))
-    (vector-set! stage (+ x (* x y)) c)
+    (vector-set! stage (+ x (* h y)) c)
     (when (= c 2) (set! nblks (add1 nblks)))
-    ;(displayln (format "[~a][~a] ~a" y x c))
+    ;(displayln (format "[~a][~a] ~a" x y c))
     (when (not (send ic is-halt?))
       (loop)))
 
   ; Part 1
   (displayln (format "Blocks: ~a, ~a, 2 = ~a" h v nblks))
   
-  (for* ([y (range 0 (add1 v))]
-         [x (range 0 (add1 h))])
-    (let ([c (vector-ref stage (+ x (* x y)))])
-      (display (format "~a" (if (= c 0) " " c))))
-    (when (= x h) (displayln "")))
+  (for ([i (range 0 1000)])
+    (when (= (modulo i 50) 0) (displayln ""))
+    (let ([c (vector-ref stage i)])
+      (when (< (modulo i 50) 46)
+      (display (format "~a" (if (= c 0) "." c)))))
+    )
+  (displayln "")
   )
 
 (part1 input)
