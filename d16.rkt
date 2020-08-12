169f0e8ce553c04b31aa2726bf1c858602c9b31e
Walter H. Yang <yangh.cn@gmail.com>
2020-08-12 10:28:50 +0800

    Day16: Optimazation
    Calcualate up-right triangle only
    
    Part 1:
    AMD R5 4800, 17s
    Intel i5 8th, 25s
    
    It's terrible.

--- d16.rkt
+++ d16.rkt
@@ -2,7 +2,12 @@
 
 (define line "12345678")
 
+; Signal/Pattern length
+(define slen 0)
+(define plen 4)
+
 (define (parse-signal line)
+  (set! slen (string-length line))
   (map (lambda (c) (string->number(string c))) (string->list line)))
 
 (define signals (parse-signal line))
@@ -14,29 +19,25 @@
            (append result (make-list n v)))
          '() pattern))
 
-;(gen-pattern 1)
-;(gen-pattern 2)
-;(gen-pattern 3)
+(define (sump sigs pat lno)
+  (foldl (lambda (idx result)
+           (let ([n (list-ref sigs idx)]
+                 [p (list-ref pat (modulo (add1 idx) plen))])
+             ;(displayln (format "nth, n * p, result: ~a ~a ~a ~a" idx n p result))
+             (+ result (* n p))))
+         0 (range lno slen)))
 
-(define (sump sig pat)
-  (let* ([slen (length sig)]
-         [plen (length pat)]
-         [out (make-list slen 0)])
-    (foldl (lambda (n idx result)
-             (let ([p (list-ref pat (modulo idx plen))])
-               ;(displayln (format "n * p, result: ~a ~a ~a" n p result))
-               (+ result (* n p))))
-           0 sig (range 1 (add1 slen)))))
-
-(define (pharse idx sigs)
-  (map (lambda (idx)
-         (modulo (abs (sump sigs (gen-pattern idx))) 10))
-       (range 1 (add1 (length sigs)))))
+(define (pharse nthloop sigs)
+  ;(displayln (format "Signals: ~a" sigs))
+  (map (lambda (lno)
+         (let ([pat (gen-pattern (add1 lno))])
+           ;(displayln (format "Pattern: ~a" pat))
+           (set! plen (length pat))
+           (modulo (abs (sump sigs pat lno)) 10)))
+       (range 0 (length sigs))))
 
 (define (fft sigs n)
-  (foldl pharse sigs (range 1 (add1 n))))
-
-;(fft (parse-signal line) 4)
+  (foldl pharse sigs (range 0 n)))
 
 (define (fft-format-8 flist)
   (list->string
@@ -44,7 +45,9 @@
           (first (string->list (number->string n))))
         flist)))
 
-(fft-format-8 (fft (parse-signal "80871224585914546619083218645595") 100))
+;(for-each (λ (n) (displayln (gen-pattern n))) (range 1 8))
 
+(fft (parse-signal line) 4)
+(fft-format-8 (fft (parse-signal "80871224585914546619083218645595") 100))
 (fft-format-8 (fft (parse-signal (first (port->lines (open-input-file "inputs/d16.txt")))) 100))
 
