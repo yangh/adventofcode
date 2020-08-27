258dcebfd8aec16ee7a0752960e14c352c0c477e
Walter H. Yang <yangh.cn@gmail.com>
2020-08-27 19:44:21 +0800

    Utils: grep add statistics info in output
    Print file/line/tool-line count at the end of output

--- 2019/grep.rkt
+++ 2019/grep.rkt
@@ -20,9 +20,12 @@
 
 (displayln (format "Search text ~a in files ~a" txt-filter file-filter))
 
+(define last-line-count 0)
+
 ; Grep file
 (define (grep file)
   (let ([lines (port->lines (open-input-file file))])
+    (set! last-line-count (length lines))
     (foldr append '()
            (map
             (λ (lno line)
@@ -32,15 +35,29 @@
             (range 1 (add1 (length lines)))
             lines))))
 
+; Match statistics
+(define file-count 0)
+(define line-count 0)
+
+(define total-line-count 0)
+
 ; Iterate files in the current directory
 (for-each
  (λ (p)
    (when (and (file-exists? p)
               (regexp-match file-filter (path->string p)))
      (let ([lines (grep p)])
+       (set! total-line-count (+ last-line-count total-line-count))
        (when (not (empty? lines))
+         (set! file-count (add1 file-count))
+         (set! line-count (+ line-count (length lines)))
          (displayln (format "> ~a" (path->string p)))
          (map displayln lines)
          (displayln "")))
      ))
  (directory-list))
+
+(displayln (format "--(~a)--(~a)--" file-filter txt-filter))
+(displayln (format "File matched: ~a" file-count))
+(displayln (format "Line matched: ~a" line-count))
+(displayln (format "Total lines: ~a" total-line-count))
