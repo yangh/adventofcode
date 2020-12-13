b23b63e8240117e945daaf67230e6e11934889db
Walter Hong Yang <yangh.cn@gmail.com>
2020-12-13 09:12:12 +0800

    update template

--- 2020/template.cpp
+++ 2020/template.cpp
@@ -8,10 +8,18 @@ using namespace std;
 
 static Tool T;
 
+static int parse_line(const string& line)
+{
+	return 0;
+}
+
 int main (int argc, char *argv[])
 {
 	Lines lines = T.input(7);
 	T.dump_lines(lines, 10);
+
+    cout << "Part 1" << endl;
+	T.fold_vector(lines, parse_line);
 
 	return 0;
 }
