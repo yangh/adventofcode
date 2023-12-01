e98e34a01947d0757ca3050d3082a2af85cfe76e
Hong YANG <hong.yang3@nio.com>
2023-12-01 15:12:29 +0800

    not finished 2021 d1

---
+++ 2021/d1.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include "../2020/tool.cpp"
+
+using namespace std;
+
+static Tool T;
+static Numbers numbers;
+
+int main (int argc, char *argv[])
+{
+	numbers = T.input_as_numbers(1);
+
+	return 0;
+}
