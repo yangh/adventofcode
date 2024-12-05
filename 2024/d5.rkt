bf532c7e2349cb0472e2f7e3aca1dc272c16a044
Hong YANG <hong.yang3@nio.com>
2024-12-05 20:54:13 +0800

    2024 d5

---
+++ 2024/d5.rkt
@@ -0,0 +1,87 @@
+#lang racket
+
+(require math/base)
+(require "utils.rkt")
+
+(define lines (input-load-lines "5"))
+
+(define print-rules (make-vector 100 '()))
+(define num-rules 0)
+
+(for ([line lines]) #:break (= 0 (string-length line))
+  ;(displayln line)
+  (set! num-rules (add1 num-rules))
+  (let* ([strs (string-split line "|")]
+         [id1 (string->number (first strs))]
+         [id2 (string->number (second strs))])
+    (vector-set! print-rules id1
+                 (append (vector-ref print-rules id1) (list id2)))))
+
+(define new-pages
+  (map (lambda (line)
+         (map string->number (string-split line ",")))
+       (take-right lines (- (length lines) num-rules 1))))
+
+(define (is-in-list? v a)
+  (if (member v a)
+      #t  ; Return true if v is found in the list
+      #f)) ; Return false if v is not found
+
+(define (valid-group v1 v2)
+  (let ([rules (vector-ref print-rules v1)])
+    (and (not (empty? rules))
+         (is-in-list? v2 rules))))
+
+;(valid-group 75 47)
+
+(define (valid-pages? pages)
+  (cond
+    ([= (length pages) 1] #t)
+    (else
+     (and
+      (let ([v1 (first pages)])
+        (andmap (lambda (v2)
+                  (valid-group v1 v2))
+                (rest pages)))
+      (valid-pages? (rest pages))))))
+
+;(valid-pages (first new-pages))
+
+(define (num-in-middle pages)
+  (list-ref pages (floor (/ (length pages) 2))))
+
+(define incorrect-pages '())
+
+(define valid-pages
+  (foldl (lambda (pages result)
+                   (append result
+                           (cond ([valid-pages? pages] (list pages))
+                                 (else
+                                  (set! incorrect-pages (append incorrect-pages (list pages)))
+                                  '()))))
+                 '() new-pages))
+
+; 4462
+(sum (map num-in-middle valid-pages))
+
+
+(define (pages-add pages page)
+  (let ([len (length pages)])
+    (let loop ([pos 0])
+      (let ([new-pages (append (take pages pos) (list page) (take-right pages (- len pos)))])
+        (cond ([valid-pages? new-pages] new-pages)
+              (else
+               (loop (add1 pos))))))))
+
+;(pages-add '(75) 97)
+;(pages-add '(97 75) 47)
+
+(define corrected-pages
+  (map (lambda (pages)
+                 (foldl (lambda (page result)
+                          (pages-add result page))
+                        '() pages))
+               incorrect-pages))
+
+; 6767
+(sum (map num-in-middle corrected-pages))
